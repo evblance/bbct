07d01e27619ab9a8eb9e0255fbd3a3c49311b923
evblance <evblance@users.noreply.github.com>
2019-05-06 13:53:09 +1000

    Mount Header component in App root

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import HomeView from './scenes/HomeView/HomeView';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Header />
       <main>
         <Route path="/" component={HomeView} exact={true} />
       </main>
